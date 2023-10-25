63d40bf9becc6333ef2e9fbf62113a59a201b230
charlesjin123 <charlesjin123@gmail.com>
2023-10-25 18:09:46 -0400

    made title fancy

--- client/src/ToxicPeople/ToxicPeoplePage.tsx
+++ client/src/ToxicPeople/ToxicPeoplePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Typography, Grid, TextField, CircularProgress } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
 import ToxicPersonCard from './ToxicPersonCard';
 import ScreenGrid from '../components/ScreenGrid';
 import IToxicPerson from '../util/types/toxicperson';
@@ -47,7 +48,16 @@ function ToxicPeoplePage() {
         spacing={2}
       >
         <Grid item>
-          <Typography variant="h2" align="center">
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: '7rem',
+              fontWeight: 'bold',
+              marginBottom: '20px',
+              color: '#ff69b4',
+              textShadow: '2px 2px 0px #fff, 4px 4px 0px rgba(0, 0, 0, 0.1)',
+            }}
+          >
             Toxic Traits
           </Typography>
         </Grid>
